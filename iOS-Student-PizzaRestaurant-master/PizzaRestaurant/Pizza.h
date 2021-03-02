e61944087f948a346a2546e6bace991dde572a31
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-03-01 22:57:39 -0800

    commit

---
+++ iOS-Student-PizzaRestaurant-master/PizzaRestaurant/Pizza.h
@@ -0,0 +1,17 @@
+//
+//  Pizza.h
+//  PizzaRestaurant
+//
+//  Created by 鈴木ちほり on 2021/03/01.
+//  Copyright © 2021 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Pizza : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Student-PizzaRestaurant-master/PizzaRestaurant/Pizza.m
@@ -0,0 +1,13 @@
+//
+//  Pizza.m
+//  PizzaRestaurant
+//
+//  Created by 鈴木ちほり on 2021/03/01.
+//  Copyright © 2021 Lighthouse Labs. All rights reserved.
+//
+
+#import "Pizza.h"
+
+@implementation Pizza
+
+@end
