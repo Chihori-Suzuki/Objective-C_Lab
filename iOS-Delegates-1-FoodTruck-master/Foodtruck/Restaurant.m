0c31f7bb98e92034f1453c9530b0e082c9cc106a
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-03-04 14:25:34 -0800

    commit

---
+++ iOS-Delegates-1-FoodTruck-master/Foodtruck/Restaurant.m
@@ -0,0 +1,13 @@
+//
+//  Restaurant.m
+//  Foodtruck
+//
+//  Created by 鈴木ちほり on 2021/03/04.
+//  Copyright © 2021 Lighthouse Labs. All rights reserved.
+//
+
+#import "Restaurant.h"
+
+@implementation Restaurant
+
+@end
